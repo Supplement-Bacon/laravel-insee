943b78df05ac85ead2261ac887b4e296e7ddadd8
Samuel Hassid <samuel@supplement-bacon.com>
2023-01-12 03:15:20 +0100

    Add default guzzle client timeout

--- src/config/insee.php
+++ src/config/insee.php
@@ -11,10 +11,12 @@ return [
     | the Insee API. You can generate your production keys by creating an
     | app and subscribe it to the Sirene API on https://api.insee.fr.
     |
-    */
+     */
 
-    'consumer_key'    => env('INSEE_CONSUMER_KEY'),
+    'consumer_key' => env('INSEE_CONSUMER_KEY'),
     'consumer_secret' => env('INSEE_CONSUMER_SECRET'),
     'store_token' => env('INSEE_STORE_TOKEN'),
+
+    'guzzle_client_timeout' => env('GUZZLE_CLIENT_TIMEOUT', 0),
 
 ];
