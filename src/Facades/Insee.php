371636385380f384f96662c2051826c636de41af
Samuel Hassid <samuel@supplement-bacon.com>
2023-01-12 02:57:34 +0100

    Add methods definition to the Facade

--- src/Facades/Insee.php
+++ src/Facades/Insee.php
@@ -4,6 +4,12 @@ namespace NSpehler\LaravelInsee\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static mixed siren(string $siren)
+ * @method static mixed siret(string $siret)
+ *
+ * @see \NSpehler\LaravelInsee\InseeClient
+ */
 class Insee extends Facade
 {
     protected static function getFacadeAccessor()
