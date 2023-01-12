2e820ef4f7663fb56224fffe2aa719851c2deb77
Samuel Hassid <samuel@supplement-bacon.com>
2023-01-12 03:27:33 +0100

    Set access_token method as private

--- src/InseeClient.php
+++ src/InseeClient.php
@@ -62,29 +62,6 @@ class InseeClient
      *
      * @param string $siren The siren number. Whitespaces are removed prior sending the request to INSEE
      */
-    public function access_token()
-    {
-        // Base64 encode consumer key and secret
-        $token = base64_encode(config('insee.consumer_key') . ':' . config('insee.consumer_secret'));
-
-        $this->headers = [
-            'headers' => [
-                'Content-Type' => 'application/x-www-form-urlencoded',
-                'Authorization' => 'Basic ' . $token,
-            ],
-            'form_params' => [
-                'grant_type' => 'client_credentials',
-            ],
-        ];
-
-        $result = $this->post(self::ENDPOINT_TOKEN);
-        // dd($result);
-
-        $result = json_decode($result->getBody());
-
-        return $result->access_token;
-    }
-
     public function siren($siren)
     {
         // Format number
@@ -112,6 +89,32 @@ class InseeClient
         $result = $this->get(self::ENDPOINT_SIRENE_V3 . '/siret/' . $siret);
 
         return json_decode($result->getBody());
+    }
+
+    /**
+     * Request access token from Insee
+     */
+    private function access_token()
+    {
+        // Base64 encode consumer key and secret
+        $token = base64_encode(config('insee.consumer_key') . ':' . config('insee.consumer_secret'));
+
+        $this->headers = [
+            'headers' => [
+                'Content-Type' => 'application/x-www-form-urlencoded',
+                'Authorization' => 'Basic ' . $token,
+            ],
+            'form_params' => [
+                'grant_type' => 'client_credentials',
+            ],
+        ];
+
+        $result = $this->post(self::ENDPOINT_TOKEN);
+        // dd($result);
+
+        $result = json_decode($result->getBody());
+
+        return $result->access_token;
     }
 
     private function requiresAuth()
