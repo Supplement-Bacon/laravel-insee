1284197fe3c01b3ad197e7e775611d40805fe5ce
Samuel Hassid <samuel@supplement-bacon.com>
2023-02-02 22:16:50 +0100

    Add commands
    To issue and prune AuthorizationToken

---
+++ src/Console/Commands/IssueInseeAccessToken.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace NSpehler\LaravelInsee\Commands\Sevenrooms;
+
+use Illuminate\Console\Command;
+use NSpehler\LaravelInsee\Insee;
+
+class IssueInseeAccessToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'insee:issue-token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Issue an Insee access token and store it';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Let\'s issue a brand new Insee access token and persist it securely');
+        $this->newLine();
+
+        Insee::access_token(store:true);
+
+        $this->info('New access token is stored, see you soon ...');
+        return 0;
+    }
+}
---
+++ src/Console/Commands/PruneInseeAccessToken.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace NSpehler\LaravelInsee\Commands\Sevenrooms;
+
+use App\Sevenrooms\Models\AuthorizationToken;
+use Illuminate\Console\Command;
+
+class PruneInseeAccessToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'insee:prune-tokens';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Prune the expired access tokens';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Let\'s issue a brand new Insee access token and persist it securely');
+        $this->newLine();
+
+        AuthorizationToken::where('expires_at', '<', now())->delete();
+
+        $this->info('Expired tokens pruned, well done ;)');
+        return 0;
+    }
+}
